6eceded0617eabe3c3d6cb3d220265521efa8a80
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-12-20 14:19:32 +0900

    user-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,8 @@
 class GroupsController < ApplicationController
   def index
+    if user_signed_in?
+    else
+    redirect_to users_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,29 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     redirect to root path
   end
+  
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destoroy
+    user = User.find(params[:id])
+    user.destoroy
+    redirect_to root_path
+  end
 
 
 private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email,:region)
   end
 end
