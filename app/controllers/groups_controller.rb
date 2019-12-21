43e5e9646645296cb69b1ebcbd3e1bc9bb3651ce
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-12-21 19:28:34 +0900

    message-view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.limit(20).order('created_at DESC')
+    @group = Group.first
   end
 
   def new
@@ -17,6 +19,6 @@ class GroupsController < ApplicationController
 private
 
   def group_params
-    params.require(:group).permit(:name, :image)
+    params.require(:group).permit(:name, :image).merge(user_id: current_user.id)
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,21 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.limit(20).order('created_at DESC')
+    @message = Message.first
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.create(message_params)
+    redirect_to messages_path
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content,:text,:image).merge(user_id: current_user.id)
   end
 end
