171c647e3e61f1446e8fca3d3fcfdb5bd205acb9
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-12-26 16:54:59 +0900

    deproy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  server '18.178.51.140', user: 'ec2-user', roles: %w{app db web}
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
