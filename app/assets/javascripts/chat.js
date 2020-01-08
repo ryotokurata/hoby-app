b1cb3bf4304821ff889b483e1220a1fd89ab1102
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2020-01-08 12:39:29 +0900

    hidouki

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,9 +1,7 @@
 $(function(){
   $(document).ready(function(){
     function buildCphat(chat){
-      
-        var html =
-        `
+        var html = `
         <div class="chat-box-content__text">
         <div class="user-name">
         ${chat.user_name}
@@ -15,12 +13,14 @@ $(function(){
         <div class="created-at">
         ${chat.date}
         </div>
+        </div>
+        <div class="more">
+        <div class="more_list">
+        <form class="button_to" method="post" action="/groups/${chat.group_id}/messages/${chat.message_id}/chats/${chat.id}"><input type="hidden" name="_method" value="delete"><button class="button" type="submit">削除
+        </button><input type="hidden" name="authenticity_token" value="Vcl5Q8qhU6KEUkwSdMH5pcEBgvEz7ZUosRhAOfvEz1p6ViZkiMzfe6tYKbxftRKUcK+Ll9JKPmRbZLh9eCjTAQ=="></form></div>
         </div>`
         return html;
-      
-  
     };
-  
     $('#new_chat').on('submit', function(e){
       e.preventDefault();
       var chat = new FormData(this);
@@ -41,7 +41,6 @@ $(function(){
           $('.chat-box-content').animate({
             scrollTop: $('.chat-box-content')[0].scrollHeight}, "fast");
           })
-          
       .fail(function(){
         alert('投稿がありません');
       })
--- app/views/chats/create.json.jbuilder
+++ app/views/chats/create.json.jbuilder
@@ -2,3 +2,5 @@ json.id      @chat.id
 json.comment @chat.comment 
 json.date    @chat.created_at.strftime("%Y/%m/%d %H:%M")
 json.user_name @chat.user.name
+json.message_id @chat.message_id
+json.group_id @chat.group_id
