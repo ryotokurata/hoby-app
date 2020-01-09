e32d24c9d89828bcb5c7cda498f3e756671731da
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2020-01-09 19:15:12 +0900

    spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a name" do
+      user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+     user.valid?
+     expect(user.errors[:nickname]).to include("can't be blank")
     end
   end
 end
