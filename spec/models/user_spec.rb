b9075ee2bf2f3e4ab5ddf476446600d11b55511d
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2020-01-10 10:41:12 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
     it "is invalid without a name" do
       user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
      user.valid?
-     expect(user.errors[:nickname]).to include("can't be blank")
+     expect(user.errors[:name]).to include("can't be blank")
     end
   end
 end
